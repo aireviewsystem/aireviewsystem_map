3edd17e5d4af24a7a521532eeb5ef282cfd99349
manthanscc <manthanscc@gmail.com>
2025-07-31 15:39:59 +0530

    update: add google tag in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
 import { AdminDashboard } from './components/AdminDashboard';
 import { CompactReviewCardView } from './components/CompactReviewCardView';
 import { LoginPage } from './components/LoginPage';
@@ -7,7 +9,13 @@ import { ProtectedRoute } from './components/ProtectedRoute';
 import { storage } from './utils/storage';
 import { auth } from './utils/auth';
 
+ReactGA.initialize("G-J7T5QPZPQ9"); // your measurement ID
+
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <Router>
       <Routes>
